8affe938f8ca0bca966d6d3381356c0134ac9cee
Leonard Apanasevich <apana@fnal.gov>
2016-11-16 01:35:37 +0100

    update script

--- ChiAnalysis/bin/SubmitMC/copyFileToFNAL.sh
+++ ChiAnalysis/bin/SubmitMC/copyFileToFNAL.sh
@@ -4,16 +4,13 @@
 EXEC=xrdcp
 
 inPREFIX=root://eoscms.cern.ch/
-inDir=/eos/cms/store/caf/user/apana/Chi_13TeV/ChiNtuples/MC/76x/
+inDir=/eos/cms/store/caf/user/apana/Chi_13TeV/ChiNtuples/MC/80x/
 
 ## FNAL
 outPREFIX=root://cmseos.fnal.gov/
-outDIR=/eos/uscms/store/user/apana/Chi_13TeV/ChiNtuples/MC/76x/
+outDIR=/eos/uscms/store/user/apana/Chi_13TeV/ChiNtuples/MC/80x/
 
-## for FILENAME in chiNtuple_Pythia8_Pt_170toInf_CB2_AK4SF_noTrees.root chiNtuple_Pythia8_Pt_170toInf_GS_AK4SF_noTrees.root
-## for FILENAME in chiNtuple_Pythia8_Pt_170toInf_CB2_noTrees.root chiNtuple_Pythia8_Pt_170toInf_GS_noTrees.root
-## for FILENAME in chiNtuple_Madgraph_Ht_300toInf_CB2_AK4SF_noTrees.root chiNtuple_Madgraph_Ht_300toInf_GS_AK4SF_noTrees.root
-for FILENAME in chiNtuple_Madgraph_Ht_300toInf_CB2_AK4SF_noTrees.root chiNtuple_Madgraph_Ht_300toInf_GS_AK4SF_noTrees.root chiNtuple_Madgraph_Ht_300toInf_GS_noTrees.root chiNtuple_Madgraph_Ht_300toInf_CB2_noTrees.root
+for FILENAME in chiNtuple_25nsMC10_flatQCD_CB2_AK4SF.root
 do
     inFILE=$inPREFIX/$inDir/$FILENAME    
     outFILE=$outPREFIX/$outDIR/$FILENAME
